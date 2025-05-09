1e4a7719d61ba03a40ce926cdf8e358e34a1e6e3
잉여개발기 <min08101@naver.com>
2025-05-09 18:26:59 +0900

    임시 추가

--- route/api_w_comment.go
+++ route/api_w_comment.go
@@ -41,20 +41,24 @@ func Api_w_comment(config tool.Config) string {
 
     db_code := tool.Get_document_setting(db, other_set["doc_name"], "document_comment_code", "")
     
-    /*
     db_code_str := ""
     if len(db_code) >= 1 {
         db_code_str = db_code[0][0]
     }
 
     if db_code_str == "" {
-        db_code_str = Api_bbs_w_comment_make(other_set["doc_name"])
+        db_code_str = Api_bbs_w_comment_make(db, other_set["doc_name"])
+
+        tool.Exec_DB(
+            db,
+            "insert into data_set (doc_name, doc_rev, set_name, set_data) values (?, '', 'document_comment_code', ?)",
+            other_set["doc_name"], db_code_str,
+        )
     }
-    */
 
     return_data := make(map[string]any)
     return_data["response"] = "ok"
-    return_data["data"] = db_code
+    return_data["data"] = db_code_str
 
     json_data, _ := json.Marshal(return_data)
     return string(json_data)
