d7dcf9d7c49ee779ca270ef5fb0a3b8f86324fd0
잉여개발기 (SPDV) <min08101@naver.com>
2025-08-10 03:15:10 +0900

    임시 추가

---
+++ route/view_bbs_in.go
@@ -0,0 +1,35 @@
+package route
+
+import (
+	"opennamu/route/tool"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+func View_bbs_in(config tool.Config) string {
+	db := tool.DB_connect()
+	defer tool.DB_close(db)
+
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+
+    other_set := map[string]string{}
+	json.Unmarshal([]byte(config.Other_set), &other_set)
+
+    bbs_name := ""
+
+    tool.QueryRow_DB(
+        db,
+        tool.DB_change("select set_data from bbs_set where set_id = ? and set_name = 'bbs_name'"),
+        []any{ &bbs_name },
+        other_set["bbs_num"],
+    )
+
+    out := tool.Get_template(db, config, bbs_name, "")
+
+    return_data := make(map[string]any)
+    return_data["response"] = "ok"
+    return_data["data"] = out
+
+    json_data, _ := json.Marshal(return_data)
+    return string(json_data)
+}
