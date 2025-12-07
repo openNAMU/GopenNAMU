22de547b35938f04c60049f08471f1ef3b2fc371
잉여개발기 <min08101@naver.com>
2025-12-07 12:15:39 +0900

    일부 추가

--- route/tool/init.go
+++ route/tool/init.go
@@ -6,11 +6,15 @@ import (
 )
 
 func Main_init() {
-	version_file_path := filepath.Join("..", "data", "version.json")
-	if _, err := os.Stat(version_file_path); err == nil {
-		// 파일 있음
-	} else {
+	if Get_now_version() == "" {
 		First_init()
+	} else {
+		now_version := Get_now_version()
+		last_version := Get_last_version()
+
+		if now_version != last_version["c_ver"] {
+			Make_set_json()
+		}
 	}
 
 	DB_init_standalone()
@@ -33,8 +37,18 @@ func Get_last_version() map[string]string {
 	}
 }
 
-func Get_now_version() {
+func Get_now_version() string {
+	version_file_path := filepath.Join("..", "data", "version.json")
+	if _, err := os.Stat(version_file_path); err == nil {
+		data, err := os.ReadFile(version_file_path)
+		if err != nil {
+			panic(err)
+		}
 
+		return string(data)
+	} else {
+		return ""
+	}
 }
 
 func First_init() {
